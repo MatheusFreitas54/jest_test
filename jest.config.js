d4d3cd7956d8f386f76a01a0165cab6edeb007d5
Matheus Freitas <matheusrlcm@gmail.com>
2025-06-24 22:48:00 -0300

    add: sonar and stryker

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/__test__/**/*.test.ts'],
+  testResultsProcessor: "jest-sonar-reporter",
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+      isolatedModules: true
+    }
+  }
 };
+
+
 
 // /** @type {import('ts-jest').JestConfigWithTsJest} */
 // module.exports = {
